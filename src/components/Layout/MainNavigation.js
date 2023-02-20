440d8636e57d4f5af0ed2885ae45b52905e0ab65
Ajay481 <iamajayyadav481@gmail.com>
2023-02-20 13:00:05 +0530

    Logout Added

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,14 +1,18 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   const authContex = useContext(AuthContext);
   const isLoggedIn = authContex.isLoggedIn;
+  const history = useHistory()
+  
   const logoutHandler = () => {
     authContex.logout();
+    history.replace("/")
   };
   return (
     <header className={classes.header}>
